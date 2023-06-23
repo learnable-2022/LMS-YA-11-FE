ef029d3a4e293a94133598a13830676a9f191bbc
John Okorie <ameachijohn001@gmail.com>
2023-06-23 09:06:58 +0100

    refactor all auth and landing pages into the codebase

--- academix/src/index.js
+++ academix/src/index.js
@@ -1,27 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-
-import theme from './styles/theme';
-
-import LandingPage from './pages/landing/landingPage';
-
-
-
-
-
-
-
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <LandingPage/>
-      </CssBaseline>
-    </ThemeProvider>
-  </React.StrictMode>
-);
-
-
+root.render(<App />);
